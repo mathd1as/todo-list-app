6a374890200458a5827c55fd7c374469c3f83696
mathd1as <matheus.vex@gmail.com>
2020-05-23 01:29:44 -0300

    Primeiro cometa :)

---
+++ lib/models/item.dart
@@ -0,0 +1,15 @@
+class Item {
+  String title;
+  bool done;
+
+  Item({this.title, this.done});
+
+  Item.fromJson(Map<String, dynamic> json );
+
+  Map<String, dynamic> toJson(){
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
+}
